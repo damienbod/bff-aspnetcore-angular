437cc5344df511790fef5c2a4f3c16ab186c6ff6
damienbod <damien_bod@hotmail.com>
2023-09-02 07:56:57 +0200

    add user profile to the angular app

--- ui/src/app/home.component.ts
+++ ui/src/app/home.component.ts
@@ -2,26 +2,42 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: 'home.component.html',
+  imports: [ CommonModule ],
 })
 export class HomeComponent implements OnInit {
-  //dataFromAzureProtectedApi$: Observable<any>;
+  dataFromAzureProtectedApi$: Observable<any>;
+  userProfileClaims: any;
   isAuthenticated = false;
   constructor(
     private httpClient: HttpClient
   ) {}
 
   ngOnInit() {
-      console.warn('home component');
+      console.info('home component');
+      this.getUserProfile();
+  }
+
+  getUserProfile() {
+    let data: any = this.httpClient
+      .get('https://localhost:5001/api/User')
+      .subscribe((result) => {
+        data = result;
+        console.log(result);
+
+        if(data.isAuthenticated) this.isAuthenticated = true;
+        this.userProfileClaims = data;
+      });
   }
 
   callApi() {
-    //this.dataFromAzureProtectedApi$ = this.httpClient
-    //  .get('https://localhost:5001/api/DirectApi')
-    //  .pipe(catchError((error) => of(error)));
+    this.dataFromAzureProtectedApi$ = this.httpClient
+      .get('https://localhost:5001/api/DirectApi')
+      .pipe(catchError((error) => of(error)));
   }
 }
