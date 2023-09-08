4b077be73c944ac1ff32997718e2bb690c239bd5
damienbod <damien_bod@hotmail.com>
2023-09-08 09:16:05 +0200

    code clean up

--- server/RejectSessionCookieWhenAccountNotInCacheEvents.cs
+++ server/RejectSessionCookieWhenAccountNotInCacheEvents.cs
@@ -2,42 +2,42 @@
 using Microsoft.Identity.Client;
 using Microsoft.Identity.Web;
 
-namespace BffAzureAD.Server
+namespace BffAzureAD.Server;
+
+public class RejectSessionCookieWhenAccountNotInCacheEvents : CookieAuthenticationEvents
 {
-    public class RejectSessionCookieWhenAccountNotInCacheEvents : CookieAuthenticationEvents
+    private readonly string[] _downstreamScopes;
+
+    public RejectSessionCookieWhenAccountNotInCacheEvents(string[] downstreamScopes)
     {
-        private readonly string[] _downstreamScopes;
+        _downstreamScopes = downstreamScopes;
+    }
 
-        public RejectSessionCookieWhenAccountNotInCacheEvents(string[] downstreamScopes)
+    public async override Task ValidatePrincipal(CookieValidatePrincipalContext context)
+    {
+        try
         {
-            _downstreamScopes = downstreamScopes;
-        }
+            var tokenAcquisition = context.HttpContext.RequestServices
+                .GetRequiredService<ITokenAcquisition>();
 
-        public async override Task ValidatePrincipal(CookieValidatePrincipalContext context)
-        {
-            try
-            {
-                var tokenAcquisition = context.HttpContext.RequestServices.GetRequiredService<ITokenAcquisition>();
-                string token = await tokenAcquisition.GetAccessTokenForUserAsync(
-                    scopes: _downstreamScopes,
-                    user: context.Principal);
-            }
-            catch (MicrosoftIdentityWebChallengeUserException ex)
-               when (AccountDoesNotExitInTokenCache(ex))
-            {
-                context.RejectPrincipal();
-            }
+            string token = await tokenAcquisition.GetAccessTokenForUserAsync(
+                scopes: _downstreamScopes, user: context.Principal);
         }
+        catch (MicrosoftIdentityWebChallengeUserException ex)
+           when (AccountDoesNotExitInTokenCache(ex))
+        {
+            context.RejectPrincipal();
+        }
+    }
 
-        /// <summary>
-        /// Is the exception thrown because there is no account in the token cache?
-        /// </summary>
-        /// <param name="ex">Exception thrown by <see cref="ITokenAcquisition"/>.GetTokenForXX methods.</param>
-        /// <returns>A boolean telling if the exception was about not having an account in the cache</returns>
-        private static bool AccountDoesNotExitInTokenCache(MicrosoftIdentityWebChallengeUserException ex)
-        {
-            return ex.InnerException is MsalUiRequiredException 
-                && (ex.InnerException as MsalUiRequiredException)!.ErrorCode == "user_null";
-        }
+    /// <summary>
+    /// Is the exception thrown because there is no account in the token cache?
+    /// </summary>
+    /// <param name="ex">Exception thrown by <see cref="ITokenAcquisition"/>.GetTokenForXX methods.</param>
+    /// <returns>A boolean telling if the exception was about not having an account in the cache</returns>
+    private static bool AccountDoesNotExitInTokenCache(MicrosoftIdentityWebChallengeUserException ex)
+    {
+        return ex.InnerException is MsalUiRequiredException 
+            && (ex.InnerException as MsalUiRequiredException)!.ErrorCode == "user_null";
     }
 }
