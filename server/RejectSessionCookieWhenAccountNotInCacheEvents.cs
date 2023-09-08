2a7dcd240d1e050dd8b4aae49241d824391d88ea
damienbod <damien_bod@hotmail.com>
2023-09-08 11:36:27 +0200

    code clean up

--- server/RejectSessionCookieWhenAccountNotInCacheEvents.cs
+++ server/RejectSessionCookieWhenAccountNotInCacheEvents.cs
@@ -20,21 +20,15 @@ public class RejectSessionCookieWhenAccountNotInCacheEvents : CookieAuthenticati
             var tokenAcquisition = context.HttpContext.RequestServices
                 .GetRequiredService<ITokenAcquisition>();
 
-            string token = await tokenAcquisition.GetAccessTokenForUserAsync(
-                scopes: _downstreamScopes, user: context.Principal);
+            string token = await tokenAcquisition.GetAccessTokenForUserAsync(scopes: _downstreamScopes, 
+                user: context.Principal);
         }
-        catch (MicrosoftIdentityWebChallengeUserException ex)
-           when (AccountDoesNotExitInTokenCache(ex))
+        catch (MicrosoftIdentityWebChallengeUserException ex) when (AccountDoesNotExitInTokenCache(ex))
         {
             context.RejectPrincipal();
         }
     }
 
-    /// <summary>
-    /// Is the exception thrown because there is no account in the token cache?
-    /// </summary>
-    /// <param name="ex">Exception thrown by <see cref="ITokenAcquisition"/>.GetTokenForXX methods.</param>
-    /// <returns>A boolean telling if the exception was about not having an account in the cache</returns>
     private static bool AccountDoesNotExitInTokenCache(MicrosoftIdentityWebChallengeUserException ex)
     {
         return ex.InnerException is MsalUiRequiredException 
